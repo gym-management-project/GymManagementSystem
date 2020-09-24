ff78915ef43addb53f1ed33fc358c5b821094f1e
shray0027 <59202921+shray0027@users.noreply.github.com>
2020-09-24 13:33:40 +0530

    added adduser page info forms

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const app =  express();
 
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded ({extended : true}));
+app.use(express.static("public"));
 
 app.get("/subscriptions",(req,res) => {
-  res.render("index");
+  res.render("subscriptions");
 });
+
+app.get("/addusers",(req,res) => {
+  res.render("addUser");
+});
+
 
 app.listen(3000 , () =>{
   console.log("server is running on port 3000");
