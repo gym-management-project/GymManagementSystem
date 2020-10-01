fee17e4550c33bd4a1f5db9140444ae359e01d78
shray0027 <59202921+shray0027@users.noreply.github.com>
2020-10-01 19:27:05 +0530

    added user information cards

--- app.js
+++ app.js
@@ -2,15 +2,64 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =require("ejs");
 const _  = require("lodash");
-
+const mongoose =require("mongoose");
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
 const app =  express();
+
+const userSchema = {
+     fname : String,
+     lname : String,
+     pnum1 : Number,
+     pnum2 : Number,
+     hno : String,
+     city : String,
+     dob : String,
+     sex : String,
+     email : String,
+     profilepic : {
+        data: Buffer,
+        contentType: String,
+    },
+     famName : String,
+     relation : String,
+     famPnum : Number,
+     boneIngury : String,
+     disease : String,
+     package : Number,
+     type : String,
+     startDate : Date,
+}
+
+
+
+
+mongoose.connect("mongodb://localhost:27017/subscriberDB",{useNewUrlParser :true ,useUnifiedTopology : true});
+const User = mongoose.model("User",userSchema);
+
+const storage = multer.diskStorage({
+    destination: "./public/uploads/",
+    filename: (req, file, cb) => {
+        var filenam =file.fieldname + '-' + Date.now()+path.extname(file.originalname);
+        cb(null, filenam);
+    }
+});
+
+const upload = multer({ storage: storage }).single("profilepic");
+
 
 app.set("view engine" , "ejs");
 app.use(bodyParser.urlencoded ({extended : true}));
 app.use(express.static("public"));
 
 app.get("/subscriptions",(req,res) => {
-  res.render("subscriptions");
+  User.find({},(err,users) => {
+     res.render("Subscriptions",{
+         users : users
+     });
+     console.log(users);
+  });
 });
 
 app.get("/addusers",(req,res) => {
@@ -23,11 +72,43 @@ app.get("/user/calculator",(req,res) => {
 });
 
 
-app.post("/addusers",(req,res) => {
-  var num = req.body;
-  console.log(num);
+app.post("/addusers",upload,(req,res,next) => {
+     const user = new User({
+        fname : _.capitalize(req.body.fname) ,
+        lname : _.capitalize(req.body.lname),
+        pnum1 : req.body.pnum1,
+        pnum2 : req.body.pnum2,
+        hno : req.body.hno,
+        city : req.body.city,
+        dob : req.body.dob,
+        sex : req.body.sex,
+        email : req.body.email,
+        profilepic : {
+            data: fs.readFileSync(path.join(__dirname + '/public/uploads/' + req.file.filename)),
+            contentType: 'image/png' || 'image/jpeg' || 'image/jpg'
+        },
+        famName : req.body.famName,
+        relation : req.body.relation,
+        famPnum : req.body.famPnum,
+        boneIngury : req.body.boneIngury,
+        disease : req.body.disease,
+        package : req.body.package,
+        type : req.body.type,
+        startDate : req.body.startDate
+     });
+     user.save((err) =>{
+       if(err){
+         console.log(err);
+       }
+       else{
+         console.log("inserted successfully");
+         res.redirect("/subscriptions")
+       }
+     });
 });
+app.post("/subscriptions",(req,res) => {
 
+});
 
 app.listen(3000 , () =>{
   console.log("server is running on port 3000");
