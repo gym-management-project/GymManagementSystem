58d3d8f8a66c9d5f56a641520717048c2ff9316c
shray0027 <59202921+shray0027@users.noreply.github.com>
2020-10-09 13:29:44 +0530

    fixed bug

--- app.js
+++ app.js
@@ -204,7 +204,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
+
 
 app.listen(port, () =>{
   console.log("server is running on port 3000");
